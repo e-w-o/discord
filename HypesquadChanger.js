d3f9f2923a6886fd28a0e6a97e76b3ae9a9caef7
ewo <100088306+e-w-o@users.noreply.github.com>
2022-06-25 14:10:55 -0400

    Create HypesquadChanger.js

---
+++ HypesquadChanger.js
@@ -0,0 +1,3 @@
+const hypesquad = require("hypesquad-changer");
+console.log(hypesquad.changer("House", "Account Token"))
+//Hypesquad Houses: Bravery, Brilliance, Balance, and also you can make it Random
